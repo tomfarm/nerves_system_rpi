2e237a976d8c5bac396604a045c83bdc8223b49d
Thomas Winkler <tom@formrausch.com>
2016-08-10 20:53:49 +0200

    how do names work?

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule NervesSystemRpi.Mixfile do
     |> String.strip
 
   def project do
-    [app: :nerves_system_rpi,
+    [app: :"nerves_system_rpi-cups",
      version: @version,
      elixir: "~> 1.2",
      compilers: Mix.compilers ++ [:nerves_system],
