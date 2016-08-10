c5738269497424b7b828a2d01f7a26da4a0f138f
Thomas Winkler <tom@formrausch.com>
2016-08-10 20:51:28 +0200

    missing ],

--- nerves.exs
+++ nerves.exs
@@ -10,6 +10,7 @@ config :nerves_system_rpi, :nerves_env,
   version: version,
   mirrors: [
     "http://udo.formrausch.com.s3.amazonaws.com/tom/nerves_system_rpi.fw"
+  ],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
