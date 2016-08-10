7b29765e65c1c3042f17feef9617f79ffc502f6a
Thomas Winkler <tom@formrausch.com>
2016-08-10 20:46:59 +0200

    changed fw dl url

--- nerves.exs
+++ nerves.exs
@@ -9,8 +9,7 @@ config :nerves_system_rpi, :nerves_env,
   type: :system,
   version: version,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_rpi/releases/download/v#{version}/nerves_system_rpi-v#{version}.tar.gz",
-    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_rpi-#{version}.tar.gz"],
+    "http://udo.formrausch.com.s3.amazonaws.com/tom/nerves_system_rpi.fw"
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
